1ad7fe70e2b80e0f622178d8aa87c23c8f98b04c
Diego <Diego@DESKTOP-159QJNC>
2023-03-30 12:17:39 +0200

    Este ejemplo te saluda

---
+++ Programa1/src/ies/entornos/EjemploGitHub.java
@@ -0,0 +1,13 @@
+package ies.entornos;
+
+public class EjemploGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		saludar();
+	}
+	public static void saludar() {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+}
